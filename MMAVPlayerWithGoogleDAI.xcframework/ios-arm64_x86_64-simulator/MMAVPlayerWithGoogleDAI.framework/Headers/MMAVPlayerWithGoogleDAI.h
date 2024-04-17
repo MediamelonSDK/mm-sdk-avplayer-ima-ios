40639567bc450ca864ef4202a193dc7bca0c52aa
Kata Venkata Sai Krishna <venkata.krishna@mediamelon.com>
2024-04-17 11:53:29 +0530

    initial commit

---
+++ MMAVPlayerWithGoogleDAI.xcframework/ios-arm64_x86_64-simulator/MMAVPlayerWithGoogleDAI.framework/Headers/MMAVPlayerWithGoogleDAI.h
@@ -0,0 +1,22 @@
+//
+//  MMAVPlayerWithGoogleDAI.h
+//  MMAVPlayerWithGoogleDAI
+//
+//  Created by Anurag Singh on 6/2/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//#import <MediaMelonSmartStreaming.h>
+
+//! Project version number for MMAVPlayerWithGoogleDAI.
+FOUNDATION_EXPORT double MMAVPlayerWithGoogleDAIVersionNumber;
+
+//! Project version string for MMAVPlayerWithGoogleDAI.
+FOUNDATION_EXPORT const unsigned char MMAVPlayerWithGoogleDAIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MMAVPlayerWithGoogleDAI/PublicHeader.h>
+
+
+#import <MMAVPlayerWithGoogleDAI/objc_mmsmartstreaming.h>
+#import <MMAVPlayerWithGoogleDAI/MediaMelonSmartStreaming.h>
